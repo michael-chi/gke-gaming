669a1621cee618be811220cf2f2de8009c38ebed
michael-chi <kalschi@gmail.com>
2020-03-17 03:12:58 +0000

    update and fix bugs

--- gke-version/game-core/utils/spanner.js
+++ gke-version/game-core/utils/spanner.js
@@ -1,4 +1,5 @@
 const {Spanner}  = require('@google-cloud/spanner');
+const User = require('../models/user');
 
 Array.prototype.toString = function(){
     const temp = [];
@@ -7,12 +8,14 @@ Array.prototype.toString = function(){
 }
 function default_read_formatter(row) {
     const result = row.toJSON({wrapNumbers:true});
-    return {
-        id: result['id'],
-        username: result['username'],
-        rank: parseInt(result['rank'].value),
-        xp: parseInt(result['xp'].value)
-    };
+    return new User(
+        id = result['id'],
+        name = result['username'],
+        lv = parseInt(result['playerLv'].value),
+        hp = parseInt(result['hp'].value),
+        mp = parseInt(result['mp'].value),
+        playerClass = result['playerClass']
+    );
 }
 function default_write_formatter(row) {
     const result = row.toJSON({wrapNumbers:true});
@@ -35,17 +38,17 @@ module.exports = class CloudSpanner {
         this.database = this.instance.database(databaseId);
     }
 
-    async readPlayer(playerId) {
+    async readPlayer(name) {
         var query = null;
-        if(playerId instanceof Array){
+        if(name instanceof Array){
             query = {
-                sql: `select * from players where id in (${playerId.toString()})`
+                sql: `select * from players where name in (${name.toString()})`
             };
         }else{
             query = {
-                sql: `select * from players where id=@playerId`,
+                sql: `select * from players where name=@name`,
                 params: {
-                    playerId: playerId,
+                    name: name,
                   },
             };
         }
@@ -59,7 +62,34 @@ module.exports = class CloudSpanner {
     }
 
     updatePlayer(player){
-
+        this.database.runTransaction(async (err, transaction) => {
+            if (err) {
+                console.error('here' + err);
+                return;
+            }
+            try {
+                console.log(`${player.name}|${player.id}|${player.playerClass}|${player.playerLv}|${player.hp}|${player.mp}`);
+                console.log(player);
+                const [rowCount] = await transaction.runUpdate({
+                    //sql: `INSERT into players(id, name, playerClass, playerLv, hp, mp) values(@id, @name, @playerClass, @playerLv, @hp, @mp)`,
+                    sql: `update players set playerLv=@playerLv, hp=@hp, mp=@mp where name=@name and id=@id`,
+                    params: {
+                      id: player.id,
+                      name: player.name,
+                      playerClass: player.playerClass,
+                      playerLv: player.playerLv,
+                      hp: player.hp,
+                      mp: player.mp
+                    }
+                  });
+                
+                await transaction.commit();
+            } catch (err) {
+                console.error('ERROR:', err);
+            } finally {
+                this.database.close();
+            }
+        });
     }
 
     async newPlayer(player) {
